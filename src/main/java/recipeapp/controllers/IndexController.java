178f54493aa6f4591e4b96377011441f21b00859
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-09 15:43:18 -0300

    Added query methods

--- src/main/java/recipeapp/controllers/IndexController.java
+++ src/main/java/recipeapp/controllers/IndexController.java
@@ -1,15 +1,38 @@
 package recipeapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import recipeapp.domain.Category;
+import recipeapp.domain.UnitOfMeasure;
+import recipeapp.repositories.CategoryRepository;
+import recipeapp.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping({"", "/", "/index"})
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @GetMapping
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("Unit of Measure ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 
--- src/main/java/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package recipeapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import recipeapp.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
