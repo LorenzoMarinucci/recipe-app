f0356cdbf013d5ae3cd7f0125e1b64be0c661b0f
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-09 13:21:14 -0300

    Created UnitOfMeasure Entity

--- src/main/java/recipeapp/domain/Ingredient.java
+++ src/main/java/recipeapp/domain/Ingredient.java
@@ -9,10 +9,11 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -47,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
