1d6f3dcab5659dab59248301ec1452ee7c9da9bb
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-12 11:20:30 -0300

    Added IndexController test

---
+++ src/test/java/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,39 @@
+package recipeapp.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import recipeapp.services.RecipeService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class IndexControllerTest {
+
+    private IndexController indexController;
+
+    @Mock
+    private RecipeService recipeService;
+
+    @Mock
+    private Model model;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        assertEquals("index", indexController.getIndexPage(model));
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
