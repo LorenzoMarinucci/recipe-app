d29b486a78afe1b7cee196ea3dd60ad111f6e721
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-17 19:34:40 -0300

    Added front-end for uploading a recipe image

--- src/test/java/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/recipeapp/controllers/IndexControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import recipeapp.domain.Recipe;
